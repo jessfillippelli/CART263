a620859f6c68fe3ea038040b1cc891986dd9e9d8
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-01-14 16:05:13 -0500

    ex from lecture

--- jan14/js/script.js
+++ jan14/js/script.js
@@ -1,8 +1,32 @@
 "use strict";
 
-window.onload = setup;
+
+// CAN DO EITHER OR
+window.addEventListener('load',setup);
+// window.onload = setup;
 
 function setup() {
-  console.log("The page has loaded!");
-  console.log("Now we can do stuff with the DOM of the page!");
+
+
+setTimeout(enlargePage,2000);
+
+//   let heading = document.getElementById('main-heading');
+// heading.style.color = 'yellow';
+// heading.style.backgroundColor = 'black';
+//   let imagesDiv = document.getElementById('images');
+// let gameIcons = imagesDiv.getElementsByClassName('game-icon');
+// // gameIcons contains an array (kind of) containing all the elements
+// // with a class of "game-icon"
+// // If there aren't any the (kind of) array will have a .length of 0
+// for (let i = 0; i < gameIcons.length; i++) {
+//   console.log(gameIcons[i]);
+// }
+  // let mainHeading = document.getElementById('main-heading');
+  // console.log(mainHeading);
+  // console.log(document);
+  // console.log("The page has loaded!");
+  // console.log("Now we can do stuff with the DOM of the page!");
+}
+function enlargePage(){
+  document.body.style.fontSize = "3em";
 }
