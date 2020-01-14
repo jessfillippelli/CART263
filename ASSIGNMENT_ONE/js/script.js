6a17c1f192a89ffc869e5ba88885b40e51293f4a
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-01-14 16:51:07 -0500

    painting step

--- ASSIGNMENT_ONE/js/script.js
+++ ASSIGNMENT_ONE/js/script.js
@@ -18,8 +18,19 @@ window.onload = setup;
 function setup(){
   console.log("is this shit working or nah");
   for (let i = 0; i < 1000; i++) {
+    //create a pixel
     let pixel = document.createElement('div');
     pixel.setAttribute('class','pixel');
+    //add a event to a pixel
+    pixel.addEventListener('mouseover', paint);
     document.body.appendChild(pixel);
+
   }
+
+
+}
+
+function paint(e){
+let pixel = e.target;
+pixel.style.backgroundColor = 'white';
 }
