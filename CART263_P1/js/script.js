5d46afff4b9552c00b1db18cad559a1facaedc4a
jessfilippelli <jessicafilippelli@outlook.com>
2020-01-20 14:12:42 -0500

    project one

---
+++ CART263_P1/js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/********************************************************************
+
+Title of Project
+Author Name
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+window.onload = setup;
+//rockSpeed = 0;
+
+//window.requestAnimationFrame();
+
+function setup(){
+  console.log("is this shit working or nah");
+  let triangle = document.createElement('');
+  let rock = (canvas.Context);
+  let context = canvas.getContext('2d');
+
+
+   context.fillstyle = "brown";
+   context.beginPath();
+   context.moveTo(500,100);
+   context.lineTo(100,175);
+   context.lineTo(101, 215);
+
+
+ let rock = rock.getContext("2d");
+ rock.beginPath();
+ rock.arc(95,50,40,0,2*Math.PI);
+ rock.fillstyle();
+
+  }
+
+  function rock(){
+    let rock = e.target;
+    let rock = document.getElementsByClassName('rock');
+  }
+
+
+function climb(){
+  let rock = document.getElementById("animate"); 
+}
