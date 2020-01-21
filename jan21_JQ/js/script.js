83564da5991f5c0cc8300b3940e61c1e82b7862f
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-01-21 14:32:14 -0500

    .slideToggle()

--- jan21_JQ/js/script.js
+++ jan21_JQ/js/script.js
@@ -13,8 +13,10 @@ to match your project! Write JavaScript to do amazing things below!
 $(document).ready(setup);
 function setup() {
   console.log("u loaded");
+$('div').slideToggle();
+
 //this
-$('div').hide().fadeIn(2000);
+// $('div').hide().fadeIn(2000);
 
   //or this
   // let $divs = $('#first');
