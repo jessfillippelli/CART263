a4f2a2094d8b3681ae193e707f547530db627607
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-01-21 15:39:10 -0500

    function update

--- acitity_JAN21/js/script.js
+++ acitity_JAN21/js/script.js
@@ -9,3 +9,12 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+$(document).ready(setup);
+function setup() {
+  setInterval(update, 500);
+}
+
+function update(){
+  console.log("Update!");
+}
