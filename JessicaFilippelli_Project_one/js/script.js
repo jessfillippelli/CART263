f6ccb845fd8365b9e9cb254b3df55c8ac7329e09
jessfilippelli <jessicafilippelli@outlook.com>
2020-01-22 15:33:36 -0500

    Update script.js
    made a var to go on the next question when the right answer is click(let currentQuestionNum =0;)
    
    and
    
    added the click() for when you click the correct answer something. it is showed it is being click in the console but it is doing nothing on the screen at the moment.

--- JessicaFilippelli_Project_one/js/script.js
+++ JessicaFilippelli_Project_one/js/script.js
@@ -11,14 +11,18 @@ to match your project! Write JavaScript to do amazing things below!
 *********************************************************************/
 
 $(document).ready(setup);
+
 let count=7;
+//helps to go on two the next question in the array
+let currentQuestionNum =0;
+
 let godQuestion = [{
  question:"am I A GOD?",
  correct: "yes",
  incorrect: "no",
 
 }, {
-question:"am I A GOD?",
+question:"am I A GOD?e",
 correct: "yes",
 incorrect: "no",
 
@@ -63,5 +67,21 @@ incorrect: "no",
 ];
 
 function setup(){
+  console.log("etattatatta")
+  $("#question").text(godQuestion[currentQuestionNum].question);
+  $("#correct").text(godQuestion[currentQuestionNum].correct);
+  $("#incorrect").text(godQuestion[currentQuestionNum].incorrect);
 
+  $( "#correct" ).click(function() {
+    event.preventDefault();
+    console.log("clicked button");
+
+    //now we move on to the next question
+    currentQuestionNum++;
+    $("#question").text(godQuestion[currentQuestionNum].question);
+    $("#correct").text(godQuestion[currentQuestionNum].correct);
+    $("#incorrect").text(godQuestion[currentQuestionNum].incorrect);
+
+
+  });
 }
