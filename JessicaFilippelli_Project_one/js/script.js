22a7cb4bb17d171632bb88b473e7ee4922f1e6bb
jessfilippelli <jessicafilippelli@outlook.com>
2020-01-22 14:50:22 -0500

    started JS

--- JessicaFilippelli_Project_one/js/script.js
+++ JessicaFilippelli_Project_one/js/script.js
@@ -9,3 +9,5 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+$(document).ready(setup);
