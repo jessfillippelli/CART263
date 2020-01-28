3f01b608447e337f8045d030dcfc0f500048bbe2
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-01-28 14:53:16 -0500

    buttons

--- JQUERY_UI_EX/js/script.js
+++ JQUERY_UI_EX/js/script.js
@@ -11,34 +11,45 @@ to match your project! Write JavaScript to do amazing things below!
 *********************************************************************/
 
 $(document).ready(setup);
-const ANIMATION_TIME = 2000;
+// const ANIMATION_TIME = 2000;
 function setup() {
 
 //dialog
 
-$('#question').dialog();
-
-
-
-
-
-$('.square').draggable({
-  stop: function(event, ui){
-  $(this).draggable('disable');
-}
-});
-//ami colors
-
-$('.square').animate({
-  backgroundColor: 'green'
-},{
-  duration: ANIMATION_TIME,
-  done: function () {
-    $(this).animate({
-      backgroundColor: 'red'
-    },ANIMATION_TIME);
+$('#question').dialog({
+  //buttons
+  buttons: {
+    "Red": makeItRed,
+    "Green": makeItGreen
   }
 });
+
+function makeItRed(){
+  $('body').css('backgroundColor', 'Red');
+}
+
+function makeItGreen(){
+  $('body').css('backgroundColor', 'Green');
+}
+
+
+// $('.square').draggable({
+//   stop: function(event, ui){
+//   $(this).draggable('disable');
+// }
+// });
+// //ami colors
+//
+// $('.square').animate({
+//   backgroundColor: 'green'
+// },{
+//   duration: ANIMATION_TIME,
+//   done: function () {
+//     $(this).animate({
+//       backgroundColor: 'red'
+//     },ANIMATION_TIME);
+//   }
+// });
 // const ANIMATION_TIME = 5000;
 // $('.square').animate({
 //   backgroundColor: 'yellow'
