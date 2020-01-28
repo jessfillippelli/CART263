206cabf1245d9a9f1a82b6b5eb2a9022b38c0802
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-01-28 14:06:40 -0500

    .draggable(); ex

--- JQUERY_UI_EX/js/script.js
+++ JQUERY_UI_EX/js/script.js
@@ -9,3 +9,9 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+$(document).ready(setup);
+
+function setup() {
+  $('.square').draggable();
+}
