64082191918e5088927e5ce1df070444b00dbfc4
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-01-28 15:24:23 -0500

    let

--- bug_ex_week4/js/script.js
+++ bug_ex_week4/js/script.js
@@ -9,3 +9,13 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+$(document).ready(setup);
+
+let $fly;
+let $mouth;
+
+function setup(){
+  $fly = $('#mouth');
+  $mouth = $('#fly');
+}
