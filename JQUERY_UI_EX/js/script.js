689a086551fded7f8bcd2f74c58117b86e596a94
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-01-28 14:35:46 -0500

    ami colors

--- JQUERY_UI_EX/js/script.js
+++ JQUERY_UI_EX/js/script.js
@@ -20,8 +20,22 @@ $('.square').draggable({
   $(this).draggable('disable');
 }
 });
-
-
+//ami colors
+const ANIMATION_TIME = 2000;
+$('.square').animate({
+  backgroundColor: 'green'
+},{
+  duration: ANIMATION_TIME,
+  done: function () {
+    $(this).animate({
+      backgroundColor: 'red'
+    },ANIMATION_TIME);
+  }
+});
+// const ANIMATION_TIME = 5000;
+// $('.square').animate({
+//   backgroundColor: 'yellow'
+// },ANIMATION_TIME);
 
 
   // $('.square').draggable({
