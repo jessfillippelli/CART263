ab6cc27f1f3c148a2b536db7888220b6b4fd3f80
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-02-11 14:01:58 -0500

    feb 11 voices

---
+++ feb_11_voices/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/********************************************************************
+
+Title of Project
+Author Name
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+
+$(document).ready(setup);
+
+
+function setup() {
+
+$(document).on('click',talk)
+
+
+}
+
+function talk(){
+  responsiveVoice.speak("hello world");
+}
