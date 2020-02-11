72ee7dd815bbff3fbe0c70de084fababe348f642
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-02-11 16:17:51 -0500

    all done

--- feb_11_activity/js/script.js
+++ feb_11_activity/js/script.js
@@ -1,50 +1,192 @@
 "use strict";
 
-/********************************************************************
+let animals = [    "aardvark",
+      "alligator",
+      "alpaca",
+      "antelope",
+      "ape",
+      "armadillo",
+      "baboon",
+      "badger",
+      "bat",
+      "bear",
+      "beaver",
+      "bison",
+      "boar",
+      "buffalo",
+      "bull",
+      "camel",
+      "canary",
+      "capybara",
+      "cat",
+      "chameleon",
+      "cheetah",
+      "chimpanzee",
+      "chinchilla",
+      "chipmunk",
+      "cougar",
+      "cow",
+      "coyote",
+      "crocodile",
+      "crow",
+      "deer",
+      "dingo",
+      "dog",
+      "donkey",
+      "dromedary",
+      "elephant",
+      "elk",
+      "ewe",
+      "ferret",
+      "finch",
+      "fish",
+      "fox",
+      "frog",
+      "gazelle",
+      "gila monster",
+      "giraffe",
+      "gnu",
+      "goat",
+      "gopher",
+      "gorilla",
+      "grizzly bear",
+      "ground hog",
+      "guinea pig",
+      "hamster",
+      "hedgehog",
+      "hippopotamus",
+      "hog",
+      "horse",
+      "hyena",
+      "ibex",
+      "iguana",
+      "impala",
+      "jackal",
+      "jaguar",
+      "kangaroo",
+      "koala",
+      "lamb",
+      "lemur",
+      "leopard",
+      "lion",
+      "lizard",
+      "llama",
+      "lynx",
+      "mandrill",
+      "marmoset",
+      "mink",
+      "mole",
+      "mongoose",
+      "monkey",
+      "moose",
+      "mountain goat",
+      "mouse",
+      "mule",
+      "muskrat",
+      "mustang",
+      "mynah bird",
+      "newt",
+      "ocelot",
+      "opossum",
+      "orangutan",
+      "oryx",
+      "otter",
+      "ox",
+      "panda",
+      "panther",
+      "parakeet",
+      "parrot",
+      "pig",
+      "platypus",
+      "polar bear",
+      "porcupine",
+      "porpoise",
+      "prairie dog",
+      "puma",
+      "rabbit",
+      "raccoon",
+      "ram",
+      "rat",
+      "reindeer",
+      "reptile",
+      "rhinoceros",
+      "salamander",
+      "seal",
+      "sheep",
+      "shrew",
+      "silver fox",
+      "skunk",
+      "sloth",
+      "snake",
+      "squirrel",
+      "tapir",
+      "tiger",
+      "toad",
+      "turtle",
+      "walrus",
+      "warthog",
+      "weasel",
+      "whale",
+      "wildcat",
+      "wolf",
+      "wolverine",
+      "wombat",
+      "woodchuck",
+      "yak",
+      "zebra"
+    ]
 
-Title of Project
-Author Name
 
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
+let answers = [];
+const NUM_OPTIONS = 5;
 
-*********************************************************************/
+
+let correctAnimal;
 $(document).ready(setup);
-let speech = ["hello people of planent ear"]
 
-function setup() {
-  responsiveVoice.speak("Hello world", "UK English Male", {
-    onstart: showSpeaking,
-    onend: hideSpeaking
-  });
+function setup(){
+newRound();
 }
 
-function showSpeaking() {
-  $('body').css('background-color', 'green');
+function newRound(){
+  answers = [];
+  for (let i = 0; i < NUM_OPTIONS; i++) {
+       let answer = animals[Math.floor(Math.random() * animals.length)];
+       addButton(answer);
+       answers.push(answer);
+}
+   correctAnimal = answers[Math.floor(Math.random() * answers.length)];
+
+sayBackwards(correctAnimal);
+
+
+
+} // end
+
+
+ function addButton(label){
+   let $button = $('<div></div>');
+   $button.addClass("guess");
+   $button.text(label);
+   $button.button();
+   $button.on( "click", handler );
+   $('body').append($button);
+ }
+
+function handler(){
+if ($(this).text() == correctAnimal){
+     $('.guess').remove();
+     setTimeout(newRound, 100);
+
+}
+else ($('.guess').effect('shake'));
+//sayBackwards(correctAnimal);
+
 }
 
-function hideSpeaking() {
-  $('body').css('background-color', 'white');
+function sayBackwards(text){
+  let backwardsText = text.split('').reverse().join('');
+   let options = {rate: 1, pitch:1 };
+
+  responsiveVoice.speak(backwardsText, "UK English Male", options);
 }
-// $(document).ready(setup);
-//
-//
-// function setup() {
-//
-// $(document).on('click',talk)
-//
-//
-// }
-//
-// function talk(){
-//   //responsiveVoice.speak("Baby shark, doo doo doo doo doo doo");
-//   responsiveVoice.speak("hello world", "UK English Male",{
-//
-//     pitch: 0,
-//     rate: 0.2
-//   }
-//
-//
-//
-// );
-// }
