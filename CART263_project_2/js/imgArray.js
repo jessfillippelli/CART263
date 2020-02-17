84771cc1fff2b772ebac031c2dec5c4d40f988e4
jessfilippelli <jessicafilippelli@outlook.com>
2020-02-17 14:55:37 -0500

    P2: STARTED PROJECT 2
    Things I did so far.
    
    Add three clown image(will be replaced) as the image to click on main page.
    
    array of images: when you click on a clown a image a image will pop up on the screen in a random spot

---
+++ CART263_project_2/js/imgArray.js
@@ -0,0 +1,21 @@
+
+
+let imgArray =[
+img[0]='assets/images/1.png',
+img[1]='assets/images/2.png',
+img[2]='assets/images/3.png',
+img[3]='assets/images/4.png',
+img[4]='assets/images/5.png',
+img[5]='assets/images/6.png',
+img[6]='assets/images/7.png',
+img[7]='assets/images/8.png',
+img[8]='assets/images/9.png',
+img[9]='assets/images/10.png',
+img[10]='assets/images/11.png',
+img[11]='assets/images/12.png',
+img[12]='assets/images/13.png',
+img[13]='assets/images/14.png',
+img[14]='assets/images/15.png',
+img[15]='assets/images/16.png',
+img[16]='assets/images/17.png'
+];
---
+++ CART263_project_2/js/script.js
@@ -0,0 +1,97 @@
+"use strict";
+
+class Photo{
+   constructor(x,y,imageSrc){
+    this.x = x;
+    this.y = y;
+    this.imageSrc = imageSrc;
+
+//create element
+    this.photoBox = document.createElement("img");
+    this.photoBox.src = this.imageSrc;
+    document.getElementById("container").appendChild(this.photoBox);
+//over write the style
+    this.photoBox.className = "photo_style";
+    this.photoBox.style.left = this.x+"px";
+    this.photoBox.style.top = this.y+"px";
+
+   }
+   } //end of photo class
+
+
+$(document).ready(setup);
+
+
+
+const MAX_PHOTOS = 16;
+let imgArrayone =[
+'assets/images/1.png',
+'assets/images/2.png',
+'assets/images/3.png',
+'assets/images/4.png',
+'assets/images/5.png',
+'assets/images/6.png',
+'assets/images/7.png',
+'assets/images/8.png',
+'assets/images/9.png',
+'assets/images/10.png',
+'assets/images/11.png',
+'assets/images/12.png',
+'assets/images/13.png',
+'assets/images/14.png',
+'assets/images/15.png',
+'assets/images/16.png'
+];
+
+
+let $one;
+let $two;
+let $three;
+
+//img = new imgArray[16];
+let img = 0;
+//let imgArray = [16];
+let photoArray =[];
+
+function setup() {
+
+  // let img = document.getElementById("imgArrayone");
+  let imageIndex = 1;
+
+
+$one = $("#one");
+$two = $("#two");
+$three = $("#three");
+
+$( "#one" ).click(function() {
+  //console.log("1 you working?");
+
+changeImage();
+}); // END OF ONE
+
+
+$( "#two" ).click(function() {
+  //console.log(" 2 you working?");
+changeImage();
+});
+
+
+$( "#three" ).click(function() {
+  //console.log(" 3 you working?");
+changeImage();
+});
+
+function changeImage(){
+
+  let index = Math.floor(Math.random()* imgArrayone.length);
+  let x = Math.floor(Math.random()*800);
+   let y = Math.floor(Math.random()*500);
+  let imageSrc = imgArrayone[index];
+  console.log("clicked "+imageSrc);
+  photoArray.push(new Photo(x,y,imageSrc));
+
+}
+
+
+
+} // end of function
