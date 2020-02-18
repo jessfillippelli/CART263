7fb52deb057fc5beab62c8d9b39d17c58acad43d
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-02-18 15:10:28 -0500

    sup

--- pizzicato/js/script.js
+++ pizzicato/js/script.js
@@ -1,37 +1,44 @@
 "use strict";
 
-/*****************
+const frequencies = [
+16.35,
+25.96,
+21.83,
+41.20,
+49.00,
+92.50,
+698.46
+];
 
-Title of Project
-Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+let notes =[];
+let synth = new Pizzicato.Sound({
+  source: 'wave'
+});
 
-******************/
+//sound files
+let kick = new Pizzicato.Sound('./assets/sounds/kick.wav');
+let snare = new Pizzicato.Sound('./assets/sounds/snare.wav');
+let hihat = new Pizzicato.Sound('./assets/sounds/hihat.wav');
 
-// preload()
-//
-// Description of preload
 
 function preload() {
 
 }
 
-
-// setup()
-//
-// Description of setup
-
 function setup() {
-
 }
 
+function mousePressed(){
+    setInterval(playNote, 500);
+    console.log("hi");
+}
 
-// draw()
-//
-// Description of draw()
+function playNote(){
+let frequency = frequencies[Math.floor(Math.random() * frequencies.length)];
+  synth.frequency = frequency;
+  synth.play();
+}
 
 function draw() {
-
 }
