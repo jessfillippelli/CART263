eb3951edf96e2eecc6600bf7f5cbeacbbd53bd6a
jessfilippelli <jessicafilippelli@outlook.com>
2020-02-20 21:10:33 -0500

    P2: made another image array to come out of the second image on screen
    the second screen is the makeup second on youtube

--- CART263_project_2/js/script.js
+++ CART263_project_2/js/script.js
@@ -1,22 +1,22 @@
 "use strict";
 
 class Photo{
-   constructor(x,y,imageSrc){
+  constructor(x,y,imageSrc){
     this.x = x;
     this.y = y;
     this.imageSrc = imageSrc;
 
-//create element
+    //create element
     this.photoBox = document.createElement("img");
     this.photoBox.src = this.imageSrc;
     document.getElementById("wrapper").appendChild(this.photoBox);
-//over write the style
+    //over write the style
     this.photoBox.className = "photo_style";
     this.photoBox.style.left = this.x+"px";
     this.photoBox.style.top = this.y+"px";
 
-   }
-   } //end of photo class
+  }
+} //end of photo class
 
 
 $(document).ready(setup);
@@ -25,73 +25,99 @@ $(document).ready(setup);
 
 const MAX_PHOTOS = 16;
 let imgArrayone =[
-'assets/images/1.png',
-'assets/images/2.png',
-'assets/images/3.png',
-'assets/images/4.png',
-'assets/images/5.png',
-'assets/images/6.png',
-'assets/images/7.png',
-'assets/images/8.png',
-'assets/images/9.png',
-'assets/images/10.png',
-'assets/images/11.png',
-'assets/images/12.png',
-'assets/images/13.png',
-'assets/images/14.png',
-'assets/images/15.png',
-'assets/images/16.png'
+  'assets/images/1.png',
+  'assets/images/2.png',
+  'assets/images/3.png',
+  'assets/images/4.png',
+  'assets/images/5.png',
+  'assets/images/6.png',
+  'assets/images/7.png',
+  'assets/images/8.png',
+  'assets/images/9.png',
+  'assets/images/10.png',
+  'assets/images/11.png',
+  'assets/images/12.png',
+  'assets/images/13.png',
+  'assets/images/14.png',
+  'assets/images/15.png',
+  'assets/images/16.png'
 ];
+
+let imgArraytwo =[
+  'assets/images/makeup_1.jpg',
+  'assets/images/makeup_2.jpg',
+  'assets/images/makeup_3.jpg',
+  'assets/images/makeup_4.jpg',
+  'assets/images/makeup_5.jpg',
+  'assets/images/makeup_6.jpg',
+  'assets/images/makeup_7.jpg',
+  'assets/images/makeup_8.jpg',
+  'assets/images/makeup_9.jpg',
+  'assets/images/makeup_10.jpg',
+  'assets/images/makeup_11.jpg',
+  'assets/images/makeup_12.jpg',
+  'assets/images/makeup_13.jpg',
+  'assets/images/makeup_14.jpg',
+  'assets/images/makeup_15.jpg',
+  'assets/images/makeup_16.jpg'
+];
+
+
+
+
+
 
 
 let $one;
 let $two;
 let $three;
 
-//img = new imgArray[16];
+
 let img = 0;
-//let imgArray = [16];
+
 let photoArray =[];
+let photoArraytwo =[];
 
 function setup() {
 
-  // let img = document.getElementById("imgArrayone");
   let imageIndex = 1;
+  $one = $("#one");
+  $two = $("#two");
+  $three = $("#three");
+
+  $( "#one" ).click(function() {
+    changeImage();
+  }); // END OF ONE
 
 
-$one = $("#one");
-$two = $("#two");
-$three = $("#three");
-
-$( "#one" ).click(function() {
-  //console.log("1 you working?");
-
-changeImage();
-}); // END OF ONE
+  $( "#two" ).click(function() {
+    changeMakeup()
+  });
 
 
-$( "#two" ).click(function() {
-  //console.log(" 2 you working?");
-changeImage();
-});
+  $( "#three" ).click(function() {
+    changeImage();
+  });
 
+  function changeImage(){
+    let index = Math.floor(Math.random()* imgArrayone.length);
+    let x = Math.floor(Math.random()*2000);
+    let y = Math.floor(Math.random()*800);
+    let imageSrc = imgArrayone[index];
+    console.log("clicked "+imageSrc);
+    photoArray.push(new Photo(x,y,imageSrc));
 
-$( "#three" ).click(function() {
-  //console.log(" 3 you working?");
-changeImage();
-});
+  }
 
-function changeImage(){
+  function changeMakeup(){
+    let index = Math.floor(Math.random()* imgArraytwo.length);
+    let x = Math.floor(Math.random()*2000);
+    let y = Math.floor(Math.random()*800);
+    let imageSrc = imgArraytwo[index];
+    console.log("clicked "+imageSrc);
+    photoArraytwo.push(new Photo(x,y,imageSrc));
 
-  let index = Math.floor(Math.random()* imgArrayone.length);
-  let x = Math.floor(Math.random()*2000);
-   let y = Math.floor(Math.random()*800);
-  let imageSrc = imgArrayone[index];
-  console.log("clicked "+imageSrc);
-  photoArray.push(new Photo(x,y,imageSrc));
-
-}
-
+  }
 
 
 } // end of function
