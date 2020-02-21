feb6483de328a6b38fc9267f3f516d93dd274ab4
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-02-21 10:39:38 -0500

    P2:added images/ change size, array file
    i put my array in  js file of there own becaue it was taking up too space in the main js page

--- CART263_project_2/js/imgArray.js
+++ CART263_project_2/js/imgArray.js
@@ -1,21 +1,38 @@
 
+let imgArrayone =[
+  'assets/images/1.png',
+  'assets/images/2.png',
+  'assets/images/3.png',
+  'assets/images/4.png',
+  'assets/images/5.png',
+  'assets/images/6.png',
+  'assets/images/7.png',
+  'assets/images/8.png',
+  'assets/images/9.png',
+  'assets/images/10.png',
+  'assets/images/11.png',
+  'assets/images/12.png',
+  'assets/images/13.png',
+  'assets/images/14.png',
+  'assets/images/15.png',
+  'assets/images/16.png'
+];
 
-let imgArray =[
-img[0]='assets/images/1.png',
-img[1]='assets/images/2.png',
-img[2]='assets/images/3.png',
-img[3]='assets/images/4.png',
-img[4]='assets/images/5.png',
-img[5]='assets/images/6.png',
-img[6]='assets/images/7.png',
-img[7]='assets/images/8.png',
-img[8]='assets/images/9.png',
-img[9]='assets/images/10.png',
-img[10]='assets/images/11.png',
-img[11]='assets/images/12.png',
-img[12]='assets/images/13.png',
-img[13]='assets/images/14.png',
-img[14]='assets/images/15.png',
-img[15]='assets/images/16.png',
-img[16]='assets/images/17.png'
+let imgArraytwo =[
+  'assets/images/m1.png',
+  'assets/images/makeup_1.jpg',
+  'assets/images/makeup_3.jpg',
+  'assets/images/makeup_4.jpg',
+  'assets/images/makeup_5.jpg',
+  'assets/images/makeup_6.jpg',
+  'assets/images/makeup_7.jpg',
+  'assets/images/makeup_8.jpg',
+  'assets/images/makeup_9.jpg',
+  'assets/images/makeup_10.jpg',
+  'assets/images/makeup_11.jpg',
+  'assets/images/makeup_12.jpg',
+  'assets/images/makeup_13.jpg',
+  'assets/images/makeup_14.jpg',
+  'assets/images/makeup_15.jpg',
+  'assets/images/makeup_16.jpg'
 ];
--- CART263_project_2/js/script.js
+++ CART263_project_2/js/script.js
@@ -24,43 +24,10 @@ $(document).ready(setup);
 
 
 const MAX_PHOTOS = 16;
-let imgArrayone =[
-  'assets/images/1.png',
-  'assets/images/2.png',
-  'assets/images/3.png',
-  'assets/images/4.png',
-  'assets/images/5.png',
-  'assets/images/6.png',
-  'assets/images/7.png',
-  'assets/images/8.png',
-  'assets/images/9.png',
-  'assets/images/10.png',
-  'assets/images/11.png',
-  'assets/images/12.png',
-  'assets/images/13.png',
-  'assets/images/14.png',
-  'assets/images/15.png',
-  'assets/images/16.png'
-];
+const MAX_PHOTOSTWO = 16;
 
-let imgArraytwo =[
-  'assets/images/m1.png',
-  'assets/images/makeup_2.jpg',
-  'assets/images/makeup_3.jpg',
-  'assets/images/makeup_4.jpg',
-  'assets/images/makeup_5.jpg',
-  'assets/images/makeup_6.jpg',
-  'assets/images/makeup_7.jpg',
-  'assets/images/makeup_8.jpg',
-  'assets/images/makeup_9.jpg',
-  'assets/images/makeup_10.jpg',
-  'assets/images/makeup_11.jpg',
-  'assets/images/makeup_12.jpg',
-  'assets/images/makeup_13.jpg',
-  'assets/images/makeup_14.jpg',
-  'assets/images/makeup_15.jpg',
-  'assets/images/makeup_16.jpg'
-];
+
+
 
 let $one;
 let $two;
@@ -77,6 +44,7 @@ let speech = []
 function setup() {
 
   let imageIndex = 1;
+  let imageIndextwo = 1;
   $one = $("#one");
   $two = $("#two");
   $three = $("#three");
