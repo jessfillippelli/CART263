223221eb09ae1614b12b2e26a4a665634cbc7841
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-02-25 09:30:55 -0500

    p2: chnaged the images names and added images

--- JessicaFilippelli_project_2/js/imgArray.js
+++ JessicaFilippelli_project_2/js/imgArray.js
@@ -1,21 +1,21 @@
 //kids array
 let imgArrayone =[
-  'assets/images/1.png',
-  'assets/images/2.png',
-  'assets/images/3.png',
-  'assets/images/4.png',
-  'assets/images/5.png',
-  'assets/images/6.png',
-  'assets/images/7.png',
-  'assets/images/8.png',
-  'assets/images/9.png',
-  'assets/images/10.png',
-  'assets/images/11.png',
-  'assets/images/12.png',
-  'assets/images/13.png',
-  'assets/images/14.png',
-  'assets/images/15.png',
-  'assets/images/16.png'
+  'assets/images/toy_1.png',
+  'assets/images/toy_2.png',
+  'assets/images/toy_3.png',
+  'assets/images/toy_4.png',
+  'assets/images/toy_5.png',
+  'assets/images/toy_6.png',
+  'assets/images/toy_7.png',
+  'assets/images/toy_8.png',
+  'assets/images/toy_9.png',
+  'assets/images/toy_10.png',
+  'assets/images/toy_11.png',
+  'assets/images/toy_12.png',
+  'assets/images/toy_13.png',
+  'assets/images/toy_14.png',
+  'assets/images/toy_15.png',
+  'assets/images/toy_16.png'
 ];
 
 
