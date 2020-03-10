4c1b49dd075c3c00b2373d43a4324dc48ffbed73
Jessica Filippelli <31858811+jessfillippelli@users.noreply.github.com>
2020-03-10 16:36:42 -0400

    fsbdf

--- json_march10/js/script.js
+++ json_march10/js/script.js
@@ -12,7 +12,40 @@ to match your project! Write JavaScript to do amazing things below!
 
 $(document).ready(setup);
 
-
 function setup() {
+  //done call that function
+  //fail call the othe functon
+$.getJSON("data/data.json").done(dataLoaded).fail(dataNotLoaded);
+}
 
+function dataLoaded(data){
+    console.log(data.condiments);
+
+//Condiment
+let randomCondiment = getRandomArrayElement(data.condiments);
+let verb = "is";
+//tell you how long the word is
+if (randomCondiment.charAt(randomCondiment.length -1 ) === "s"){
+  verb = "are";
+}
+
+//cat
+let randomCat = getRandomArrayElement(data.cats);
+
+//room
+let randomRoom = getRandomArrayElement(data.rooms);
+
+let randomDecription = `${randomCondiment} ${verb} like a  ${randomCat} in a ${randomRoom}`;
+$('body').append(randomDecription);
+
+}//end of data
+
+function dataNotLoaded(request, text, error){
+
+}
+
+function getRandomArrayElement(array){
+  console.log(array);
+let element = array[Math.floor(Math.random()*array.length)];
+return element;
 }
