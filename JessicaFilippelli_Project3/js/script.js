8d6319c860590bf023983e881445427e21466c13
Jessica Filippelli <j_fil@cslab-blue.concordia.ca>
2020-03-11 11:03:38 -0400

    p3: workded on game two
    p3: fix dragable and made data into one big array

--- JessicaFilippelli_Project3/js/script.js
+++ JessicaFilippelli_Project3/js/script.js
@@ -11,23 +11,51 @@ to match your project! Write JavaScript to do amazing things below!
 *********************************************************************/
 
 $(document).ready(setup);
-
+  let masterArray = [];
 
 
 function setup() {
-$.getJSON("data/whoiswho.json")
+
+//get data
+$.getJSON("data/whoiswho.json",function( data ) {
+  console.log("load")
+  let items = [];
+
+  $.each( data, function( key, val ) {
+    items.push( val );
+  });
 
 
-  $( function() {
+//put data into an array
+ for(let i = 0; i<items.length; i++){
+    let tempArray = items[i];
+    for(let j = 0; j<tempArray.length;j++){
+      masterArray.push(tempArray[j])
+    }
+
+    }
+
+//start game
+  initializeGame();
+
+})
+/*.done(function() {
+    console.log( "second success" );
+  })*/
+  .fail(function(error) {
+    console.log( "error"+ error );
+  })
+
+}//end of set up
+
+function initializeGame(){
+
       $( "#list_one" ).sortable();
       $( "#list_one" ).disableSelection();
-    } );
-
-    $("#game_one").hide();
-
-    $( function() {
-        $( "#draggable" ).draggable();
+      $("#game_one").hide();
+      $( ".draggable" ).draggable();
         $( "#droppable" ).droppable({
+
           drop: function( event, ui ) {
             $( this )
               .addClass( "ui-state-highlight" )
@@ -35,10 +63,9 @@ $.getJSON("data/whoiswho.json")
                 .html( "Dropped!" );
           }
         });
-      } );
 
+        for (let i =0; i<masterArray.length;i++){
+          console.log(masterArray[i])
+        }
 
-
-
-
-}//end of set up
+      }
