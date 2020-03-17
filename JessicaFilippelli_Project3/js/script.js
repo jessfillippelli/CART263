fa8243ab257178b138b7ab05681cd95efdea7e67
jessfilippelli <jessicafilippelli@outlook.com>
2020-03-16 22:32:48 -0400

    p3: data style
    trying to get the data one after the otehr

--- JessicaFilippelli_Project3/js/script.js
+++ JessicaFilippelli_Project3/js/script.js
@@ -2,8 +2,8 @@
 
 /********************************************************************
 
-Title of Project
-Author Name
+Title of Project: Fan HP quiz
+Author Name: Jessica Filippelli
 
 This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
@@ -11,7 +11,7 @@ to match your project! Write JavaScript to do amazing things below!
 *********************************************************************/
 
 $(document).ready(setup);
-  let masterArray = [];
+let masterArray = [];
 
 
 function setup() {
@@ -33,16 +33,10 @@ $.getJSON("data/whoiswho.json",function( data ) {
       masterArray.push(tempArray[j])
     }
   } // end of for loop
-
-
-
-
-
 //start game
   initializeGame();
 
 })
-
   .fail(function(error) {
     console.log( "error"+ error );
   })
@@ -65,19 +59,27 @@ function initializeGame(){
           }
         });
 
+        //display the data text
+        let $container = $(".gametwotext");
+        //let person = getRandomPerson();
+        //$container.text(person.name + person.house + person.hair + person.blood_status + person.patronus);
+      // $container.text("person.name \n + person.house \n + person.hair \n + person.blood_status \n + person.patronus");
 
-
-    //display the data text
-      let $container = $(".gametwotext");
-      let person = getRandomPerson();
-      $container.text(person.name + person.house + person.hair + person.blood_status + person.patronus);
-          $('#game_two').append($
-          );
-
+      let person = getRandomElement(students.name);
+      let house = getRandomElement(students.house);
+      let hair = getRandomElement(students.hair);
+      let blood = getRandomElement(students.blood_status);
+      let patrons = getRandomElement(students.patronus);
+        $('#game_two').append($
+        );
       }
 
-//make the data random
-      function getRandomPerson(){
-          let randomPerson = masterArray[Math.floor(Math.random()*masterArray.length)];
-          return randomPerson;
+      // //make the data random
+      // function getRandomPerson(person.name){
+      //   // let randomPerson = masterArray[Math.floor(Math.random()*masterArray.length)];
+      //   // return randomPerson;
+      // }
+
+      function getRandomElement(array){
+        return array[Math.floor(Math.random() * array.length)];
       }
