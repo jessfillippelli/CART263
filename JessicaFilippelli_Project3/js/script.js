56ea6192be7c419d065564bdd746475803001f42
jessfilippelli <jessicafilippelli@outlook.com>
2020-03-19 09:01:39 -0400

    p3: added images

--- JessicaFilippelli_Project3/js/script.js
+++ JessicaFilippelli_Project3/js/script.js
@@ -17,27 +17,27 @@ let randomPerson;
 
 function setup() {
 
-//get data
-$.getJSON("data/whoiswho.json",function( data ) {
-  console.log("load")
-  let items = [];
+  //get data
+  $.getJSON("data/whoiswho.json",function( data ) {
+    console.log("load")
+    let items = [];
 
-  $.each( data, function( key, val ) {
-    items.push( val );
-  });
+    $.each( data, function( key, val ) {
+      items.push( val );
+    });
 
 
-//put data into an array
- for(let i = 0; i<items.length; i++){ //start of for loop
-    let tempArray = items[i];
-    for(let j = 0; j<tempArray.length;j++){
-      people.push(tempArray[j])
-    }
-  } // end of for loop
-//start game
-  initializeGame();
+    //put data into an array
+    for(let i = 0; i<items.length; i++){ //start of for loop
+      let tempArray = items[i];
+      for(let j = 0; j<tempArray.length;j++){
+        people.push(tempArray[j])
+      }
+    } // end of for loop
+    //start game
+    initializeGame();
 
-})
+  })
   .fail(function(error) {
     console.log( "error"+ error );
   })
@@ -46,37 +46,37 @@ $.getJSON("data/whoiswho.json",function( data ) {
 
 function initializeGame(){
 
-      // $( "#list_one" ).sortable();
-      $( "#list_one" ).disableSelection();
-      $("#game_one").hide();
-      $( ".draggable" ).draggable();
-        $( "#droppable" ).droppable({
+  // $( "#list_one" ).sortable();
+  $( "#list_one" ).disableSelection();
+  $("#game_one").hide();
+  $( ".draggable" ).draggable();
+  $( "#droppable" ).droppable({
 
-//where to relize what is the correct name is
-//for evverything single image
-//can comparie in drop
-//acc name with image class with that Name
-//can check name with class
-//every image can have the right name
-          drop: function( event, ui ) {
-            $( this )
-              .addClass( "ui-state-highlight" )
-              .find( "div" )
-                .html( "Dropped!" );
-          }
-        });
+    //where to relize what is the correct name is
+    //for evverything single image
+    //can comparie in drop
+    //acc name with image class with that Name
+    //can check name with class
+    //every image can have the right name
+    drop: function( event, ui ) {
+      $( this )
+      .addClass( "ui-state-highlight" )
+      .find( "div" )
+      .html( "Dropped!" );
+    }
+  });
 
-        //display the data text
-        let $container = $(".gametwotext");
-        // get a random stuent from the students array
-        randomPerson = getRandomElement(people);
-        console.log(randomPerson);
+  //display the data text
+  let $container = $(".gametwotext");
+  // get a random stuent from the students array
+  randomPerson = getRandomElement(people);
+  console.log(randomPerson);
 
-      //  $('#game_two').append($);
-      //  see in the index.html where the container is coming from
-$container.html("Name: "+randomPerson.Name + "<br> "  + "House: "+randomPerson.House + "<br> " + "Hair: " + randomPerson.Hair + "<br> " + "Blood status: "+randomPerson.Blood_status + "<br> "  + "Patronus: "+randomPerson.Patronus);
-      }
+  //  $('#game_two').append($);
+  //  see in the index.html where the container is coming from
+  $container.html("Name: "+randomPerson.Name + "<br> "  + "House: "+randomPerson.House + "<br> " + "Hair: " + randomPerson.Hair + "<br> " + "Blood status: "+randomPerson.Blood_status + "<br> "  + "Patronus: "+randomPerson.Patronus);
+}
 
-      function getRandomElement(array){
-        return array[Math.floor(Math.random() * array.length)];
-      }
+function getRandomElement(array){
+  return array[Math.floor(Math.random() * array.length)];
+}
